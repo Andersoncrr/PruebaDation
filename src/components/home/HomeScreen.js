7e6f0f615bf0a542da4629b8e54b78c4a726072c
Anderson Cruz <andersoncruz24@hotmail.com>
2021-11-04 08:25:21 -0500

    ajuste buscador y likes

--- src/components/home/HomeScreen.js
+++ src/components/home/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { startLogout } from "../../actions/auth";
 import { News } from "../../const/News";
@@ -6,30 +6,49 @@ import "./HomeScreen.css";
 
 export const HomeScreen = () => {
   const dispatch = useDispatch();
+  const [liked, setLiked] = useState(null);
   const handleLogout = () => {
     dispatch(startLogout());
   };
 
+  const searchText = () => {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+      a = li[i];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
   const listItems = News.map((item) => (
-    <div className="container " style={{ marginTop: "50px" }}>
-      <div className="row d-flex justify-content-center">
-        <div className="col-md-0 d-flex justify-content-center">
-          <div className="card-sl ">
-            <div className="card-image d-flex justify-content-center">
-              <img src={item.Img} alt={item.Title} />
+    <li className="list">
+      <div className="container " style={{ marginTop: "50px" }}>
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-0 d-flex justify-content-center">
+            <div className="card-sl ">
+              <div className="card-image d-flex justify-content-center">
+                <img src={item.Img} alt={item.Title} />
+              </div>
+              
+              <div className={liked ? "card-click" : "card-action"} onClick={ () => setLiked(!liked)} >
+                <i className="fa fa-heart"></i>
+              </div>
+              <div className="card-heading">{item.Title}</div>
+              <div className="card-text">{item.Description}</div>
             </div>
-            <a
-              className="card-action"
-              href="https://freefrontend.com/bootstrap-cards/"
-            >
-              <i className="fa fa-heart"></i>
-            </a>
-            <div className="card-heading">{item.Title}</div>
-            <div className="card-text">{item.Description}</div>
           </div>
         </div>
       </div>
-    </div>
+    </li>
   ));
 
   return (
@@ -44,9 +63,9 @@ export const HomeScreen = () => {
         </div>
         <div className="formulario card-body">
           <div className="form-group row justify-content-center ">
-            <input type="text" className="form-control" />
+            <input className="form-control" type="text" id="myInput" onKeyUp={searchText} placeholder="Digita una palabra..." />
           </div>
-          <ul>{listItems}</ul>
+          <ul id="myUL">{listItems}</ul>
         </div>
       </div>
     </div>
